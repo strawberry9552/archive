45393f9bd0cf067a71c788246927912bdfbe2a0e
strawberry9552 <strawberry9552@gmail.com>
2026-05-15 17:36:18 +0900

    260515_simplyscroll

--- js/direc.js
+++ js/direc.js
@@ -10,4 +10,9 @@ $(function () {
   $(".menu li").on("mouseleave", function () {
     $(".menu li").removeClass("hover");
   });
+
+  $(".leftSlide").simplyScroll({
+    orientation: "vertical",
+    speed: 0.5,
+  });
 });
