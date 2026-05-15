8c605a952c2d9de7f30effa6f7059d72cef487a5
strawberry9552 <strawberry9552@gmail.com>
2026-05-15 17:11:40 +0900

    260515_ver2

--- js/direc.js
+++ js/direc.js
@@ -10,4 +10,13 @@ $(function () {
   $(".menu li").on("mouseleave", function () {
     $(".menu li").removeClass("hover");
   });
+
+  // $(".direcLeft").on("mouseenter", function () {
+  //   $(this).addClass("fill");
+  // });
+
+  $("#gallery").simplyScroll({
+    orientation: "vertical",
+    speed: 0.5,
+  });
 });
