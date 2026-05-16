82c0ec7c0e11a7ba9c291bdd9cc63249131cb2de
strawberry9552 <strawberry9552@gmail.com>
2026-05-16 20:09:50 +0900

    260516 업뎃

--- js/direc.js
+++ js/direc.js
@@ -1,8 +1,29 @@
 $(function () {
+<<<<<<< HEAD
   $(".menu li").on("click", function () {
     $(".menu li").removeClass("on");
     $(this).addClass("on");
   });
+=======
+  $(".pre:nth-child(1)").find("ul").simplyScroll({
+    speed: 1,
+    orientation: "vertical",
+    direction: "forwards",
+  });
+  $(".menu li").on("click", function () {
+    $(".menu li").removeClass("on");
+    $(this).addClass("on");
+    let a = $(this).index();
+    $(".pre").removeClass("on");
+    $(".pre").eq(a).addClass("on");
+    $(".pre").eq(a).find("ul").simplyScroll({
+      speed: 1,
+      orientation: "vertical",
+      direction: "forwards",
+    });
+  });
+
+>>>>>>> b58dea6 (260516 업뎃)
   $(".menu li").on("mouseenter", function () {
     $(".menu li").removeClass("hover");
     $(this).addClass("hover");
@@ -10,8 +31,11 @@ $(function () {
   $(".menu li").on("mouseleave", function () {
     $(".menu li").removeClass("hover");
   });
+<<<<<<< HEAD
   $("#leftSlide").simplyScroll({
     orientation: "vertical",
     speed: 0.5,
   });
+=======
+>>>>>>> b58dea6 (260516 업뎃)
 });
