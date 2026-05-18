5667f6b1186a25140124a5b079faa9abca7b0899
strawberry9552@gmail.com <strawberry9552@gmail.com>
2026-05-18 20:50:21 +0900

    260518 Update 2.2

---
+++ js/artwork.js
@@ -0,0 +1,22 @@
+$(function () {
+  let stw = gsap.utils.toArray(".rightBox li");
+  gsap.to(stw, {
+    yPercent: 80 * (stw.length - 8),
+
+    scrollTrigger: {
+      trigger: ".artwork",
+      start: "center center",
+      end: "+=2000",
+      scrub: 2,
+      pin: true,
+    },
+  });
+
+  $(".leftBox").on("mouseenter", function () {
+    $(".leftBox").removeClass("p_on");
+    $(this).addClass("p_on");
+  });
+  $(".leftBox").on("mouseleave", function () {
+    $(".leftBox").removeClass("p_on");
+  });
+});
